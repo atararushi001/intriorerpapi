8f2192dfb7b86ab659aa41f88b9a7b9ba6e3ed3c
Naman Thanki <stuniz7855@gmail.com>
2024-05-26 21:51:34 +0530

    no changes

--- api/server.js
+++ api/server.js
@@ -8,17 +8,15 @@ const fs = require("fs");
 require("./config/db.config");
 
 const router = require("./routes");
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/api", router);
-app.get('/', (req, res)=>{ 
-  res.status(200); 
-  res.send("Welcome to root URL of Server"); 
-}); 
+app.get("/", (req, res) => {
+	res.status(200);
+	res.send("Welcome to root URL of Server");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-  
+	console.log(`Server is running on port ${PORT}`);
+});
